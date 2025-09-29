55f51dd72b3cbfdd27367af2918b0ce2f86cd714
muhammadimmaduddinroomi-del <muhammadimmaduddinroomi@gmail.com>
2025-09-29 12:50:11 +0500

    Add files via upload

---
+++ class 4 file 2.py
@@ -0,0 +1,68 @@
+# Arthematic oprator
+# print(3+5)
+
+# a = 4
+# b = 10
+# sum = a+b
+# diff = a-b
+# mul = a*b
+# div =  b/a
+# flrdiv =b//a
+# exp = a**2
+# mod = a%b
+# print(a+b)
+# print(sum)
+# print(diff)
+# print(mul)
+# print(div)
+# print(flrdiv)
+# print(exp)
+# print("reminder",mod)
+
+# comparison opertors
+print(5.1==5.1)
+# problem 6
+a = 2
+b = 6 
+add = a+b
+sub = a-b
+div = b/a
+mul = a*b
+print("addtion is ", add)
+print("subtraction is ", sub)
+print("multiplication is ", mul)
+print("division is ", div)
+
+print()
+# prob 7
+a = 25
+b = 4
+div =a/b
+print("remainder is",div)
+print()
+# prob 8
+a =12
+square = 12**2
+print(square)
+
+print()
+
+# prob 9
+a = 15>10,15>20
+print(a)
+print()
+
+# prob 10
+a=5==5
+print(a)
+print()
+
+# prob 11
+a = 4
+even =2
+print(a%even)
+
+# prob 12
+a=5
+b=("1==10" ,or a=<10)
+print(b)
---
+++ class4date25.py
@@ -0,0 +1,63 @@
+# conversion of string to int
+# score = "100";
+# print(type(score))
+# score_int = int(score)
+# print(score_int,type(score_int))
+# print()
+# score1=(score_int+2)
+# print(score1)
+
+# convert int to float
+# x = 247
+# print(x)
+# print(type(x))
+# x = float(x)
+# print(x)
+# print(type(x))
+
+# convert float to string
+# c = 76.324
+# print(type(c))
+# c =str(c)
+# print(type(c))
+
+
+# pratice problem 1
+# x = "50"
+# print(type(x))
+# x =int(x)
+# print(type(x))
+# x = x+10
+# print(x)
+# print(type(x))
+# print()
+
+ # practice problen no 2
+# x =22.5
+# print(type(x))
+# x =str(x)
+# print(type(x))
+# print()
+
+# # practice prob 3
+# x = "45"
+# y = "99"
+# g =(x+y)
+# print(g)
+# print(type(g))
+# g =int(g)
+# print(type(g))
+# print()
+# # problem 4
+# x = 10
+# x=float(x)
+# print(x)
+
+# print(type(x))
+# print(x/4)
+# print()
+# problem number 5
+# x =3.9
+# x =int(x)
+# print(type(x))
+# print(x)
