8a4f5b78a9a08a7cafeb461d4faf3c14aac7cead
muhammadimmaduddinroomi-del <muhammadimmaduddinroomi@gmail.com>
2025-09-29 12:49:40 +0500

    Add files via upload

---
+++ sami python.py
@@ -0,0 +1,40 @@
+name = "ali"
+age = 45
+print("my name is",name, "age is",age)
+
+city = "karachi"
+print(city)
+
+x=4
+y=5
+result = x+y
+print("Result is",result)
+
+print("twinkle twinkle little star")
+line2 =( "how are wonder ")
+line3 = "what you are"
+line4 = "up above the in the sky"
+line5 = "like a dimound in the sky\n"
+
+print()
+
+hobby  = "cricket\n"
+print("my hobby is",hobby)
+
+
+name = "ali"
+age = 45
+city = "lahore"
+
+print("my name is",name, "\nage is",age, "\ncity is",city)
+
+school = "nasra-school"
+print("my school is",school)
+favourite_food ='biryani' 
+print("\nmy favourite food is",favourite_food)
+
+print()
+
+favourite_colour = "blue"
+print("\nmy favoutite colour is",favourite_colour)
+
